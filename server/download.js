d23347b4c97f7aa1215f8124e7b312645c835102
Paulo Fontes <paulomfontes@gmail.com>
2023-09-14 16:16:19 -0300

    aula 2 47

--- server/download.js
+++ server/download.js
@@ -1,26 +1,28 @@
 import ytdl from "ytdl-core"
 import fs from "fs"
 
-export const download = (videoId) => {
-  const videoURL = "https://www.youtube.com/shorts/" + videoId
-  console.log("Realizando o download do vídeo:", videoId)
+export const download = (videoId) =>
+  new Promise((resolve, reject) => {
+    const videoURL = "https://www.youtube.com/shorts/" + videoId
+    console.log("Realizando o download do vídeo:", videoId)
 
-  ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly" })
-    .on("info", (info) => {
-      const seconds = info.formats[0].approxDurationsMs / 1000
+    ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly" })
+      .on("info", (info) => {
+        const seconds = info.formats[0].approxDurationsMs / 1000
 
-      if (seconds > 60) {
-        throw new Error("A duração desse video é maior do que 60 segundos.")
-      }
-    })
-    .on("end", () => {
-      console.log("Download do vídeo finalizado!")
-    })
-    .on("error", (error) => {
-      console.log(
-        "Não foi possível fazer o download do vídeo. Detalhe do erro:",
-        error
-      )
-    })
-    .pipe(fs.createWriteStream("./tmp/audio.mp4"))
-}
+        if (seconds > 60) {
+          throw new Error("A duração desse video é maior do que 60 segundos.")
+        }
+      })
+      .on("end", () => {
+        console.log("Download do vídeo finalizado!")
+        resolve()
+      })
+      .on("error", (error) => {
+        console.log(
+          "Não foi possível fazer o download do vídeo. Detalhe do erro:",
+          error
+        )
+      })
+      .pipe(fs.createWriteStream("./tmp/audio.mp4"))
+  })
