636f5563d19d80467867215a0bb048ee9e773826
Paulo Fontes <paulomfontes@gmail.com>
2023-09-14 21:34:17 -0300

    final

--- server/download.js
+++ server/download.js
@@ -8,19 +8,19 @@ export const download = (videoId) =>
 
     ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly" })
       .on("info", (info) => {
-        const seconds = info.formats[0].approxDurationsMs / 1000
+        const seconds = info.formats[0].approxDurationMs / 1000
 
         if (seconds > 60) {
-          throw new Error("A duração desse video é maior do que 60 segundos.")
+          throw new Error("A duração desse vídeo é maior do que 60 segundos.")
         }
       })
       .on("end", () => {
-        console.log("Download do vídeo finalizado!")
+        console.log("Download do vídeo finalizado.")
         resolve()
       })
       .on("error", (error) => {
         console.log(
-          "Não foi possível fazer o download do vídeo. Detalhe do erro:",
+          "Não foi possível fazer o download do vídeo. Detalhes do erro:",
           error
         )
         reject(error)
